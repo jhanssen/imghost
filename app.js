751f67ff2cb6fda971da7713f9d70da3a54c5445
Jan Erik Hanssen <jhanssen@gmail.com>
2017-08-19 22:44:45 -0700

    more

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const passport = require("passport");
 
 const data = {
     mongoose: undefined,
+    gridfs: undefined,
     User: undefined,
     updateOptions: { upsert: true, new: true, setDefaultsOnInsert: true }
 };
@@ -52,7 +53,17 @@ function ensureAuthenticated(req, res, next) {
 
 module.exports = function(mongoose, option) {
     data.mongoose = mongoose;
-    data.User = mongoose.model("User", { email: String, displayName: String });
+    data.gridfs = require("mongoose-gridfs")({
+        collection: "fs",
+        model: "Image",
+        mongooseConnection: mongoose.connection
+    });
+    data.Image = data.gridfs.model;
+    data.User = mongoose.model("User", {
+        email: String,
+        displayName: String,
+        images: [{ type: mongoose.Schema.Types.ObjectId, ref: "Image" }]
+    });
 
     const connection = mongoose.connection;
     const sessionSecret = option("session-secret");
@@ -83,6 +94,14 @@ module.exports = function(mongoose, option) {
         res.send("Hello world");
     });
 
+    app.get("/auth", (req, res) => {
+        let out = "";
+        enabledAuths.forEach(auth => {
+            out += `<div><a href="/auth/${auth}">${auth}</a></div>`;
+        });
+        res.send(out);
+    });
+
     if (enabledAuths.indexOf("google") !== -1) {
         console.log("Setting up Google auth routes");
         app.get("/auth/google", passport.authenticate("google", { scope: ["profile","email"] }));
@@ -98,8 +117,37 @@ module.exports = function(mongoose, option) {
         res.sendFile(path.join(__dirname + "/files/upload.html"));
     });
     app.post("/images/upload", ensureAuthenticated, upload.array("photos", 12), (req, res) => {
-        console.log("woop", req.files);
-        res.send("ok");
+        const images = req.files.map(file => {
+            return file.id;
+        });
+        const remove = (msg) => {
+            data.Image.remove({ _id: { $in: images } }).then(() => {
+                res.sendStatus(500);
+            }).catch(err => {
+                // something went horribly wrong
+                console.error(msg, err);
+                res.sendStatus(500);
+            });
+        };
+        data.User.findOne({ email: req.user.email }).then(user => {
+            if (!user)
+                throw `Unable to find user ${req.user.email}`;
+            console.log("found user", user);
+            //console.log("images", images);
+            data.User.findOneAndUpdate({ email: user.email }, { $push: { images: { $each: images } } }).then(doc => {
+                if (!doc)
+                    throw `Unable to find user ${user.email} (2)`;
+                res.send("ok");
+            }).catch(err => {
+                // also bad, remove images
+                console.error("unable to update user", err);
+                remove("unable to remove images (2)");
+            });
+        }).catch(err => {
+            // this is bad, remove images
+            console.error("unable to find user", err);
+            remove("unable to remove images (1)");
+        });
     });
 
     const port = option.int("port", 3001);
